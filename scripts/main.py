21d56c6352b1e5f451bf261d9685cca9f5b77252
Benoît Quero <benoitquero@MacBook-Pro-de-Benoit.local>
2019-11-20 13:22:21 +0100

    add main

---
+++ scripts/main.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Read image
+img = cv.imread('lena.png',cv.IMREAD_COLOR)
+img_ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
+img_y = img_ycrcb[:,:,0]
+
+# Gaussian filter TO DO
+#blur = cv.blur(img_y,(5,5))
+
+# Gradient
+sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=1)
+
+cv.imshow('image',sobelx)
+cv.waitKey(0)
+cv.destroyAllWindows()
