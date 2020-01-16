1c5e42ff9100a949fc461ab8e82de514ec855dec
Vincent Berthet Swift-win <vincent.berthet42@gmail.com>
2020-01-16 09:21:02 +0100

    eigen  bgr to lab wip

--- scripts/tensors.py
+++ scripts/tensors.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import blend_modes
 import time
 
 # Parameters
@@ -8,25 +7,28 @@ start_time = time.time()
 sigma = 1       # standard deviation >=0
 alpha=10
 # Read image
-img = cv.imread('./sources/lena.png',cv.IMREAD_COLOR)
+img = cv.imread('./sources/lena.png',cv.IMREAD_COLOR) #bgr
 
 # Estimate the smoothed structure tensor field
 img_blur = cv.GaussianBlur(img,(5,5),alpha) # blur
-cv.imwrite('./output/tensors/img_blur.jpg', img_blur)
-img_sobel = cv.Sobel(img, cv.CV_64F, 1, 1, ksize=1) 
-cv.imwrite('./output/tensors/img_sobel.jpg', img_sobel)
-img_sobel_t=np.zeros((img.shape))
-cv.transpose(img_sobel,img_sobel_t)
-cv.imwrite('./output/tensors/img_sobel_t.jpg', img_sobel_t)
+cv.imwrite('./output/tensors/img_blur.jpg', img_blur) 
+#sobel x
+img_sobel_x=cv.Sobel(img,cv.CV_64F,1,0,ksize=1) 
+cv.imwrite('./output/tensors/img_sobel_x.jpg', img_sobel_x)
+#sobel y
+img_sobel_y=cv.Sobel(img,cv.CV_64F,0,1,ksize=1) 
+cv.imwrite('./output/tensors/img_sobel_y.jpg', img_sobel_y)
 
-img_sum=np.zeros((img.shape[0],img.shape[1]))
-for i in range(0,img.shape[2]) :
-    img_sum+=img_sobel[:,:,i]*img_sobel_t[:,:,i]
-
-img_smoothed=cv.GaussianBlur(img_sum,(5,5),sigma) 
-cv.imwrite('./output/tensors/img_smoothed.jpg', img_smoothed)
+#BGR->LAB conversion
+img_sobel_x_lab=np.zeros((img.shape[0],img.shape[1]))
+img_sobel_y_lab=np.zeros((img.shape[0],img.shape[1]))
+print(type(img_sobel_x))
+cv.cvtColor(img_sobel_x,img_sobel_x_lab,cv.COLOR_BGR2Lab)
+cv.cvtColor(img_sobel_y,img_sobel_y_lab,cv.COLOR_BGR2Lab)
 
 # @TODO extract eigen values/vectors (lambda/theta)
+#img_lab=np.zeros((img.shape[0],img.shape[1]))
+#cv.cvtColor(img_smoothed,img_lab,cv.COLOR_BGR2Lab)
 
 
 
