e620dbff687f92815d71e14478a6cdca4e50b9c1
Vincent Berthet Swift-win <vincent.berthet42@gmail.com>
2020-01-16 09:58:09 +0100

    time to go

--- scripts/tensors.py
+++ scripts/tensors.py
@@ -2,33 +2,35 @@ import cv2 as cv
 import numpy as np
 import time
 
-# Parameters
+## Parameters
 start_time = time.time()
-sigma = 1       # standard deviation >=0
-alpha=10
-# Read image
+sigma = 1    
+## Read image
 img = cv.imread('./sources/lena.png',cv.IMREAD_COLOR) #bgr
 
-# Estimate the smoothed structure tensor field
-img_blur = cv.GaussianBlur(img,(5,5),alpha) # blur
-cv.imwrite('./output/tensors/img_blur.jpg', img_blur) 
+## Estimate the smoothed structure tensor field
 #sobel x
 img_sobel_x=cv.Sobel(img,cv.CV_64F,1,0,ksize=1) 
 cv.imwrite('./output/tensors/img_sobel_x.jpg', img_sobel_x)
 #sobel y
 img_sobel_y=cv.Sobel(img,cv.CV_64F,0,1,ksize=1) 
 cv.imwrite('./output/tensors/img_sobel_y.jpg', img_sobel_y)
-
 #BGR->LAB conversion
-img_sobel_x_lab=np.zeros((img.shape[0],img.shape[1]))
-img_sobel_y_lab=np.zeros((img.shape[0],img.shape[1]))
-print(type(img_sobel_x))
-cv.cvtColor(img_sobel_x,img_sobel_x_lab,cv.COLOR_BGR2Lab)
-cv.cvtColor(img_sobel_y,img_sobel_y_lab,cv.COLOR_BGR2Lab)
+img_sobel_x_lab = cv.cvtColor(np.uint8(img_sobel_x), cv.COLOR_BGR2LAB)
+img_sobel_y_lab = cv.cvtColor(np.uint8(img_sobel_y), cv.COLOR_BGR2LAB)
+#compute eigen 
+A=img_sobel_x_lab[:,:,0]*img_sobel_x_lab[:,:,0]+img_sobel_x_lab[:,:,1]*img_sobel_x_lab[:,:,1]+img_sobel_x_lab[:,:,2]*img_sobel_x_lab[:,:,2]
+B=img_sobel_y_lab[:,:,0]*img_sobel_y_lab[:,:,0]+img_sobel_y_lab[:,:,1]*img_sobel_y_lab[:,:,1]+img_sobel_y_lab[:,:,2]*img_sobel_y_lab[:,:,2]
+C=img_sobel_x_lab[:,:,0]*img_sobel_y_lab[:,:,0]+img_sobel_x_lab[:,:,1]*img_sobel_y_lab[:,:,1]+img_sobel_x_lab[:,:,2]*img_sobel_y_lab[:,:,2]
+#blur
+A=cv.GaussianBlur(A,(5,5),sigma)
+B=cv.GaussianBlur(B,(5,5),sigma)
+C=cv.GaussianBlur(C,(5,5),sigma)
 
-# @TODO extract eigen values/vectors (lambda/theta)
-#img_lab=np.zeros((img.shape[0],img.shape[1]))
-#cv.cvtColor(img_smoothed,img_lab,cv.COLOR_BGR2Lab)
+
+# @TODO extract lambda +/- for each pixel ?
+print(A.shape)
+
 
 
 
