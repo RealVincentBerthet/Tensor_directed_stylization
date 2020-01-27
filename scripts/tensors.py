a586800d2a9fc472394f5af52f5da230a44fe8d9
Vincent Berthet Swift-win <vincent.berthet42@gmail.com>
2020-01-27 09:56:03 +0100

    save Lundi

--- scripts/tensors.py
+++ scripts/tensors.py
@@ -19,7 +19,7 @@ cv.imwrite('./output/tensors/img_sobel_y.jpg', img_sobel_y)
 img_sobel_x_lab = cv.cvtColor(np.uint8(img_sobel_x), cv.COLOR_BGR2LAB)
 img_sobel_y_lab = cv.cvtColor(np.uint8(img_sobel_y), cv.COLOR_BGR2LAB)
 
-#compute eigen 
+#compute eigens 
 A=img_sobel_x_lab[:,:,0]*img_sobel_x_lab[:,:,0]+img_sobel_x_lab[:,:,1]*img_sobel_x_lab[:,:,1]+img_sobel_x_lab[:,:,2]*img_sobel_x_lab[:,:,2]
 B=img_sobel_y_lab[:,:,0]*img_sobel_y_lab[:,:,0]+img_sobel_y_lab[:,:,1]*img_sobel_y_lab[:,:,1]+img_sobel_y_lab[:,:,2]*img_sobel_y_lab[:,:,2]
 C=img_sobel_x_lab[:,:,0]*img_sobel_y_lab[:,:,0]+img_sobel_x_lab[:,:,1]*img_sobel_y_lab[:,:,1]+img_sobel_x_lab[:,:,2]*img_sobel_y_lab[:,:,2]
@@ -29,25 +29,25 @@ B=cv.GaussianBlur(B,(5,5),sigma)
 C=cv.GaussianBlur(C,(5,5),sigma)
 
 #Convert A,B,C CV_32FC1
-A=np.float32(A)
-B=np.float32(B)
-C=np.float32(C)
+A=np.float64(A)
+B=np.float64(B)
+C=np.float64(C)
 
-#Extract Tensors from eigenValues, eigenVectors
+#1. Compute Tensors from eigenValues, eigenVectors
 p1=2 #p1>=p2>=0
 p2=1
-T=np.zeros(A.shape,np.float32)
+T=np.zeros(A.shape,np.float64)
 for i in range(img.shape[1]) :
     for j in range (img.shape[2]) :
         #create symetric matrix 2x2 [[A,C][C,B]]
-        tmp=np.zeros((2,2),np.float32)
+        tmp=np.zeros((2,2),np.float64)
         tmp[0,0]=A[i,j]
         tmp[0,1]=C[i,j]
         tmp[1,0]=C[i,j]
         tmp[1,1]=B[i,j]
 
-        eigen=cv.eigen(tmp)
         #extract eigenValues and eigenVectors
+        eigen=cv.eigen(tmp)
         lambda_plus=eigen[1][0]
         lambda_moins=eigen[1][1]
         theta_plus=eigen[2][0]
@@ -60,16 +60,16 @@ for i in range(img.shape[1]) :
         T[i,j]=AVERIF[0]
 
 
+#2. Decomposition champs de vecteurs
+phi=0
+angle=np.array([np.cos(phi),np.sin(phi)],np.float64)
+
+#np.multiply(np.sqrt(T),angles) #@TODO 
 
 
 
-
-
-
-
-
-
-# Compute the stroke tensor field T of the input color image I
-
+# normalizedImg= cv.normalize(T, None, 0,255, norm_type=cv.NORM_MINMAX)
+# cv.imshow('dst_rt', normalizedImg)
+# cv.waitKey(0)
 
 print('time : '+str(round(time.time() - start_time))+' seconds')
